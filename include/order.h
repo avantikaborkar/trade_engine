98b74066be41472752967dcccfa1007015ab77db
Avantika Borkar <borkaravantika01@gmail.com>
2026-03-11 09:22:36 +0530

    first commit

---
+++ include/order.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+enum class Side {
+    BUY,
+    SELL
+};
+
+struct Order {
+    int orderId;
+    int price;
+    int quantity;
+    Side side;
+
+    Order(int id, int p, int q, Side s)
+        : orderId(id), price(p), quantity(q), side(s) {}
+};
